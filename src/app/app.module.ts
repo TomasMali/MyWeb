3ea16332ff90c2e670cd9bf8ecb01a389032db7b
tommal <tmali@sanmarcoinformatica.it>
2019-03-03 18:51:16 +0100

    V_2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
+import { GithubService } from './home/github.service';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ResumeComponent,
+    WorkComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule
+
+   
   ],
-  providers: [],
+  providers: [
+    GithubService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { GithubService } from './github.service';
+
+import {  HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  userName: string= "" ;
+  responseJson: string= "";
+  response: any;
+
+  constructor(public github: GithubService, public http: HttpClient) { 
+
+  }
+
+
+  ngOnInit() {
+  }
+
+  public getMyReopos(){
+    this.http
+    .get('https://api.github.com/users/TomasMali/').subscribe((response)=> {
+      console.log(response)
+    })
+  }
+
+  public search(){
+      this.http
+      .get('https://api.github.com/users/' + this.userName).subscribe((response)=> {
+        //this.responseJson = JSON.stringify(response)
+        this.response = response;
+  })
+  }
+
+}
