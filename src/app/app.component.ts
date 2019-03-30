e0ade72ac3304a8e00eadc01c9c37e800cb49687
tommal <tommal@ubuntu.ubuntu-domain>
2019-03-30 11:34:54 +0100

    first deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,20 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
 
-// 4.
+  // 4.
   constructor(private router: Router) {
 
   }
 
 
   title = 'Resume';
+
+
+  downloadResume() {
+
+    window.open("../assets/CV-Europass-20180504-Mali-IT.pdf");
+    return false;
+
+  }
 
 }
