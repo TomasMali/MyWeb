aac730e2181d4e9c08feb41b940a2cd83142f5db
tomasmali <tomasmali08@gmail.com>
2019-03-31 09:39:18 +0200

    tomas

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/tema-one'));
+app.use(express.static(__dirname + '/dist/temaOne'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/tema-one/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/temaOne/index.html'));
 });
 
 // default Heroku port
