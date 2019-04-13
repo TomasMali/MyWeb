e0c1c472349812907177c64877467241bd2b8e0e
tomasmali <tomasmali08@gmail.com>
2019-04-13 16:31:47 +0200

    fix some issue html mobile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:     {
+    apiKey: "AIzaSyDZG2kqzzaOXBgGUU_fXvLZ_JdFbdBcKFY",
+    authDomain: "user-service-b9b59.firebaseapp.com",
+    databaseURL: "https://user-service-b9b59.firebaseio.com",
+    projectId: "user-service-b9b59",
+    storageBucket: "",
+    messagingSenderId: "578220639623"
+  }
 };
