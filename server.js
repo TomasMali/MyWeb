8fc6e856568eb9f2d64a4db54ff66eea5b10a4ae
tomasmali <megatoys92@gmail.ocm>
2019-11-28 21:34:26 +0100

    non funzionante forse

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/temaOne'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/temaOne/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/temaOne/index.php'));
 });
 
 // default Heroku port
